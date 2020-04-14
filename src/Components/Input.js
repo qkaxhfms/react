02f6f70af8d2e0efce4551a46e765d69db434906
unknown <kwakms821@daum.net>
2020-04-15 01:13:08 +0900

    add Input components

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const InputBox = styled.input`
+    width:140px;
+    line-height:40px;
+    padding:0 10px;
+    carcet:#444;
+    border:1px solid transparent;
+    border-top:1px solid #ccc;
+    border-bottom:1px solid #ccc;
+`
+
 
 function Input(){
-    return <input />;
+    return <InputBox placeholder="text here"/>;
 }
 
 export default Input;
