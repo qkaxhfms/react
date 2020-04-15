67cf2b52a0cd9b786b42b730ec40151c8e79aee0
unknown <kwakms821@daum.net>
2020-04-16 00:24:10 +0900

    start react and add Htext components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Button from "./Components/Button";
 import Input from "./Components/Input";
+import Htext from "./Components/Htext";
 
 function App() {
   return (
     <>
       <Button />
       <Input />
+      <Htext />
     </>
   );
 }
---
+++ src/Components/Htext.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './common.css';
+
+function Htext(){
+    return <h1>Hello World</h1>;
+}
+
+export default Htext;
